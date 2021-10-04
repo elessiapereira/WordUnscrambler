36b7faf9d59bb5057881d8d3d1ba8511547097a1
myriame2 <91742293+myriame2@users.noreply.github.com>
2021-10-04 10:20:16 -0400

    Add files via upload
    Change 4

--- WordUnscrambler/Program.cs
+++ WordUnscrambler/Program.cs
@@ -1,84 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WordUnscrambler
+namespace WordUnscambler
 {
-    class Program
+    class Constants
     {
-        private static readonly FileReader _fileReader = new FileReader();
-        private static readonly WordMatcher _wordMatcher = new WordMatcher();
+        public const string OptionsOnHowToEnterScrambledWords = "Enter scrambled word(s) manually or as a file: F - file/M - manual";
+        public const string OptionsOnContinuingTheProgram = "Would you like to continue? Y/N";
 
-        static void Main(string[] args)
-        {
-            try
-            {
-                Console.WriteLine("Enter scrambled word(s) manually or as a file: F - file / M - manual");
+        public const string EnterScrambledWordsViaFile = "Enter full path including the file name: ";
+        public const string EnterScrambledWordsManually = "Enter word(s) manually (separated by commas if multiple): ";
+        public const string EnterScrambledWordsOptionNotRecognized = "The option was not recognized.";
 
-                String option = Console.ReadLine() ?? throw new Exception("String is empty");
+        public const string ErrorScrambledWordsCannotBeLoaded = "Scrambled words were not loaded becasue there was an error: ";
+        public const string ErrorProgramWillBeTerminated = "The program will be terminated: ";
 
-                switch (option.ToUpper())
-                {
-                    case "F":
-                        Console.WriteLine("Enter full path including the file name: ");
-                        ExecuteScrambledWordsInFileScenario();
-                        break;
-                    case "M":
-                        Console.WriteLine("Enter word(s) manually (separated by commas if multiple): ");
-                        ExecuteScrambledWordsManualEntryScenario();
-                        break;
-                    default:
-                        Console.WriteLine("The entered option was not recognized.");
-                        break;
-                }
+        public const string MatchFound = "MATCH FOUND FOR {0}: {1}";
+        public const string MatchNotFound = "NO MATCHES HAVE BEEN FOUND";
 
-                Console.ReadLine();
+        public const string Yes = "Y";
+        public const string No = "N";
+        public const string File = "F";
 
 
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("The program will be terminated." + ex.Message);
-
-            }
-        }
-
-        private static void ExecuteScrambledWordsInFileScenario()
-        {
-            var filename = Console.ReadLine();
-            string[] scrambledWords = _fileReader.Read(filename);
-            DisplayMatchedUnscrambledWords(scrambledWords);
-        }
-
-        private static void ExecuteScrambledWordsManualEntryScenario()
-        {
-        }
-
-        private static void DisplayMatchedUnscrambledWords(string[] scrambledWords)
-        {
-            //read the list of words from the system file. 
-            string[] wordList = _fileReader.Read("wordlist.txt");
-
-            //call a word matcher method to get a list of structs of matched words.
-            List<MatchedWord> matchedWords = _wordMatcher.Match(scrambledWords, wordList);
-
-            // Change 3
-            if (matchedWords.Any())
-            {
-                foreach (var matchedWord in matchedWords)
-                {
-                    string matchFound = "A match has been found for {0}: {1}";
-
-                    Console.WriteLine(matchFound, matchedWord.ScrambledWord, matchedWord.Word);
-                }
-            }
-            else
-            {
-                string matchNotFound = "No match found";
-                Console.WriteLine(matchNotFound);
-            }
-        }
+        public const string WordListFileName = "wordlist.txt";
     }
 }
